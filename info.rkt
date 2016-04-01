1b4bf8162f764c69596c7be5a42ed6eb772967a1
William G Hatch <william@hatch.uno>
2016-04-01 15:13:31 -0600

    fix some issues of info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,9 @@
 #lang info
 
-(define deps '("base"))
+(define deps '("base"
+               "gui-lib"
+               "unix-socket-lib"
+               "xdg"))
 (define build-deps '("scribble-lib"
                      "racket-doc"))
 (define scribblings '(("doc.scrbl" () (tool))))
