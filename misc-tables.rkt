10d292d1bc5d67cec0f259696b9d4990796e078f
William G Hatch <william@hatch.uno>
2016-04-28 15:13:07 -0600

    add sunglasses emoticons

--- misc-tables.rkt
+++ misc-tables.rkt
@@ -12,4 +12,9 @@
    ;; for TAFKAP is capital letter t with hook, combining ring above,
    ;; combining short stroke overlay, and combining caron below.
    "the artist formerly known as prince" "\u01AC\u030A\u0335\u032C"
+
+   ;; For some reason I get a kick out of this emoticon series.
+   "emoticon no sunglasses" "( •_•)"
+   "emoticon putting on sunglasses" "( •_•)>⌐■-■"
+   "emoticon wearing sunglasses" "(⌐■_■)"
    ))
